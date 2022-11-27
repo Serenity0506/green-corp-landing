cf382d6656746c77f2dba742018d836523e26704
Liubov Gayduk <serenity0506@yandex.ru>
2022-11-27 22:33:25 +0300

    запустили счетчик и добавили вкладку другое с помощью js

---
+++ js/animations.js
@@ -0,0 +1,39 @@
+const INCREASE_NUMBER_ANIMATION_SPEED = 100;
+
+function increaseNumberAnimationStep(i, element, endNumber) {
+    if (i <= endNumber) {
+        if (i === endNumber) element.innerText = i + '+';
+        else element.innerText = i;
+    };
+    i += 100;
+    setTimeout(increaseNumberAnimationStep, INCREASE_NUMBER_ANIMATION_SPEED, i, element, endNumber);
+}
+
+function initIncreaseNumberAnimation() {
+    let element = document.querySelector('.features__clients-count');
+    increaseNumberAnimationStep(0, element, 5000)
+}
+initIncreaseNumberAnimation();
+  
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    if (event.target.value === 'other') {
+      const formContainer = document.createElement('div');
+      formContainer.classList.add('form__group');
+      formContainer.classList.add('form__other-input'); 
+   
+      const input = document.createElement('input');
+      input.placeholder = "Введите ваш вариант";
+      input.type = "text"; 
+        
+      formContainer.appendChild(input);
+      document.querySelector('.form form').insertBefore(formContainer, document.querySelector('.form__submit'));
+    }
+   
+    const otherInput = document.querySelector('.form__other-input');
+    if (event.target.value !== 'other' && otherInput) {
+    document.querySelector('.form form').removeChild(otherInput);
+    }
+  });
+
+  
